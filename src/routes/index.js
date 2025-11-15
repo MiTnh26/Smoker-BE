5c749e5e6a8c167f7df73cc7ae25cae4b400924f
minhtran <minhtnhe186338@fpt.edu.vn>
2025-11-15 19:30:01 +0700

    personalBooking, relevant

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,6 @@ const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const businessRoutes = require("./businessRoutes");
 const postRoutes = require("./postRoutes");
-module.exports = { authRoutes, userRoutes, businessRoutes, postRoutes };
+const bookingRoutes = require("./bookingRoutes");
+
+module.exports = { authRoutes, userRoutes, businessRoutes, postRoutes, bookingRoutes };
