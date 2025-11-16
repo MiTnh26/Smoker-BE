25d43e60362910273ba214478c37198bdd53a678
minhtran <minhtnhe186338@fpt.edu.vn>
2025-11-16 19:25:32 +0700

    fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./src/app");
 
-const port = process.env.PORT;
-const host = process.env.HOSTNAME;
+const port = process.env.PORT || 9999;
+const host = process.env.HOST || "0.0.0.0";
 
 // Create HTTP server for Socket.io
 const server = http.createServer(app);
