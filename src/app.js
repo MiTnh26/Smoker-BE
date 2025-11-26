394df1f8f30a1b4d3bbcf23c486d26ac2c914e76
dng0021 <anhle200w@gmail.com>
2025-11-26 22:12:55 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -128,11 +128,8 @@ app.use("/api/bar-table", barTableRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/business", businessRoutes);
-<<<<<<< HEAD
 app.use("/api/events", eventRoutes)
-=======
 app.use("/api/events", eventRoutes);
->>>>>>> origin/main
 app.use("/api/posts", postRoutes);
 app.use("/api/stories", storyRoutes);
 app.use("/api/bookings", bookingRoutes);
@@ -148,20 +145,17 @@ app.use("/api/reports", reportRoutes);
 app.use("/api/bank-info", bankInfoRoutes);
 app.use("/api/livestream", livestreamRoutes);
 app.use("/api/medias", mediaRoutes);
-<<<<<<< HEAD
 app.use("/api/bookingtable",bookingTableRoutes)
 
 
 
 
-=======
 app.use("/api/booking", bookingRoutes);
 app.use("/api/admin", adminRoutes);
 
 // UserReview & BarReview APIs
 app.use("/api/user-reviews", userReviewRoutes);
 app.use("/api/bar-reviews", barReviewRoutes);
->>>>>>> origin/main
 app.get("/", (req, res) => {
   res.json({ 
     message: "Welcome to Smoker API 🚬",
