3560ae3becc3b2795756fdb222c2cac90b26a468
minhtran <minhtnhe186338@fpt.edu.vn>
2025-11-27 02:23:39 +0700

    fiix

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -8,6 +8,7 @@ async function getUuidModule() {
     uuidModule = await import("uuid");
   }
   return uuidModule;
+}
 // Simple UUID (RFC 4122) validator to avoid ESM-only `uuid` package issues
 const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 function isValidUUID(value) {
