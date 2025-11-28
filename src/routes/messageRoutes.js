85a74e8d411153013096d46a4449c5a9f2be086b
khoahche173284 <khoahche173284@fpt.edu.vn>
2025-11-28 21:11:20 +0700

    loi

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -10,8 +10,11 @@ router.use(verifyToken);
 router.post('/conversation', checkBannedStatus, messageController.getOrCreateConversation);
 // Lấy danh sách cuộc trò chuyện của user
 router.get('/conversations', messageController.getUserConversations);
+<<<<<<< HEAD
 // Lấy tổng số tin nhắn chưa đọc
 router.get('/messages/unread-count', messageController.getTotalUnreadMessagesCount);
+=======
+>>>>>>> parent of 2cbdf81 (Merge branch 'duongandkhoa' of https://github.com/MiTnh26/Smoker-BE into duongandkhoa)
 // Gửi tin nhắn
 router.post('/message', checkBannedStatus, messageController.sendMessage);
 // Lấy danh sách tin nhắn của 1 cuộc trò chuyện
